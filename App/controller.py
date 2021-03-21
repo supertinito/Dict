77b75a2527a3446fefa714a9d7d4e338f320aee0
supertinito <s.rodriguez64@uniandes.edu.co>
2021-03-21 00:23:58 -0500

    cambios hoy

--- App/controller.py
+++ App/controller.py
@@ -1,38 +1,28 @@
-﻿"""
- * Copyright 2020, Departamento de sistemas y Computación,
- * Universidad de Los Andes
- *
- *
- * Desarrolado para el curso ISIS1225 - Estructuras de Datos y Algoritmos
- *
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along withthis program.  If not, see <http://www.gnu.org/licenses/>.
- """
-
+﻿
 import config as cf
 import model
 import csv
 
+def loadCategorias(catalog):
 
-"""
-El controlador se encarga de mediar entre la vista y el modelo.
-"""
+    a = cf.data_dir + 'a.txt'
 
-# Inicialización del Catálogo de libros
+    input_file = csv.DictReader(open(categoriafile, encoding='utf-8'),delimiter='\t')
+    for categoria in input_file:
+        model.addListaCategorias(catalog, categoria)
 
-# Funciones para la carga de datos
+def loadVideos(catalog):
+    videosfile = cf.data_dir + 'videos-small.csv'
+    input_file = csv.DictReader(open(videosfile, encoding='utf-8'))
+    for video in input_file:
+        model.addVideo(catalog, video)
 
-# Funciones de ordenamiento
+def PalabraSize(catalog):
 
-# Funciones de consulta sobre el catálogo
+    return model.PalabraSize(catalog)
+
+def loadData(catalog):
+
+    loadBooks(catalog)
+    loadTags(catalog)
+    loadBooksTags(catalog)
--- App/model.py
+++ App/model.py
@@ -1,28 +1,4 @@
-﻿"""
- * Copyright 2020, Departamento de sistemas y Computación,
- * Universidad de Los Andes
- *
- *
- * Desarrolado para el curso ISIS1225 - Estructuras de Datos y Algoritmos
- *
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along withthis program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Contribuciones:
- *
- * Dario Correal - Version inicial
- """
+﻿
 
 
 import config as cf
@@ -32,10 +8,31 @@ from DISClib.DataStructures import mapentry as me
 from DISClib.Algorithms.Sorting import shellsort as sa
 assert cf
 
-"""
-Se define la estructura de un catálogo de videos. El catálogo tendrá dos listas, una para los videos, otra para las categorias de
-los mismos.
-"""
+def newCatalog():
+    """ Inicializa el catálogo de libros
+
+    Crea una lista vacia para guardar todos los libros
+
+    Se crean indices (Maps) por los siguientes criterios:
+    Autores
+    ID libros
+    Tags
+    Año de publicacion
+
+    Retorna el catalogo inicializado.
+    """
+    catalog = {'Palabras':None}
+
+    """
+    Este indice crea un map cuya llave es la etiqueta
+    """
+    catalog['Palabras'] = mp.newMap(100,
+                                maptype='PROBING',
+                                loadfactor=0.5,
+                                comparefunction=compareletra
+
+    return catalog
+
 
 # Construccion de modelos
 
@@ -44,7 +41,18 @@ los mismos.
 # Funciones para creacion de datos
 
 # Funciones de consulta
-
+def PalabraSize(catalog):
+    """
+    Numero de tags en el catalogo
+    """
+    return mp.size(catalog['Palabras'])
 # Funciones utilizadas para comparar elementos dentro de una lista
-
+def compareletra(name, letra):
+    letraentry = me.getKey(letra)
+    if (name == letraentry):
+        return 0
+    elif (name > letraentry):
+        return 1
+    else:
+        return -1
 # Funciones de ordenamiento
